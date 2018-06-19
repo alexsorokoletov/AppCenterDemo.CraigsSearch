df18fd70fd70bf5951c8f24298b26fbacc82f72e
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2018-06-19 13:53:40 -0400

    iOS AppCenter integration

--- DemoCraigsSearch2/App.xaml.cs
+++ DemoCraigsSearch2/App.xaml.cs
@@ -1,4 +1,6 @@
-using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,7 +18,7 @@ namespace DemoCraigsSearch2
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("ios=175c7e80-25c4-4e97-a546-27bd3a4a266e;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
